b1287b2c4b578542c2d67cc757e1ae8cc248332a
Stephen <Stephen@38bbe8e2-6fdc-11de-9e9f-05296fd93cd0>
2007-03-26 14:13:51 +0000

    - added CLI args handling

--- trunk/CssParser/CssCompactor/Program.cs
+++ trunk/CssParser/CssCompactor/Program.cs
@@ -7,34 +7,63 @@
 
 using System;
 using System.IO;
+using System.Collections.Generic;
+using System.Reflection;
+
+using BuildTools.IO;
 
 namespace BuildTools.CssCompactor
 {
 	class Program
 	{
+		#region Constants
+
 		private const string Help =
-			"Cascading StyleSheet compactor and syntax validator\r\n\r\n"+
+			"Cascading StyleSheet compactor and syntax validator {0}\r\n\r\n"+
 			"CssCompactor /IN:file [ /OUT:file ] [ /COPY:copyright ] [ /TIME:timeFormat ] [ /P ]\r\n\r\n"+
 			"\t/IN:\tInput File Path\r\n"+
 			"\t/OUT:\tOutput File Path\r\n"+
 			"\t/COPY:\tCopyright label\r\n"+
 			"\t/TIME:\tTimeStamp Format\r\n"+
 			"\t/P\tPretty-Print the output\r\n\r\n"+
-			"e.g. CssCompactor /IN:myFile.css /OUT:myCompactFile.css /COPY:\"(c)2007 My CSS\" /TIME:\"'Compacted 'yyyy-MM-dd @ HH:mm\"";
+			"e.g. CssCompactor /IN:myFile.css /OUT:compacted/myFile.css /COPY:\"(c)2007 My CSS\" /TIME:\"'Compacted 'yyyy-MM-dd @ HH:mm\"";
+
+		public enum ArgType
+		{
+			None,// need a default value
+			InputFile,
+			OutputFile,
+			Copyright,
+			TimeStamp,
+			PrettyPrint
+		}
+
+		private static readonly ArgsTrie<ArgType> Mapping = new ArgsTrie<ArgType>(
+			new ArgsMap<ArgType>[] {
+				new ArgsMap<ArgType>("/IN:", ArgType.InputFile),
+				new ArgsMap<ArgType>("/OUT:", ArgType.OutputFile),
+				new ArgsMap<ArgType>("/COPY:", ArgType.Copyright),
+				new ArgsMap<ArgType>("/TIME:", ArgType.TimeStamp),
+				new ArgsMap<ArgType>("/P", ArgType.PrettyPrint),
+			});
+
+		#endregion Constants
 
 		static void Main(string[] args)
 		{
 			try
 			{
-				string inputFile = (args.Length > 0) ? args[0] : null;
-				string outputFile = (args.Length > 1) ? args[1] : null;
-				string copyright = (args.Length > 2) ? args[2] : null;
-				string timestamp = (args.Length > 3) ? args[3] : null;
-				string prettyPrint = (args.Length > 3) ? args[3] : null;
+				Dictionary<ArgType,string> argList = Mapping.MapAndTrimPrefixes(args);
+
+				string inputFile = argList.ContainsKey(ArgType.InputFile) ? argList[ArgType.InputFile] : null;
+				string outputFile = argList.ContainsKey(ArgType.OutputFile) ? argList[ArgType.OutputFile] : null;
+				string copyright = argList.ContainsKey(ArgType.Copyright) ? argList[ArgType.Copyright] : null;
+				string timeStamp = argList.ContainsKey(ArgType.TimeStamp) ? argList[ArgType.TimeStamp] : null;
+				bool prettyPrint = argList.ContainsKey(ArgType.PrettyPrint);
 
 				if (String.IsNullOrEmpty(inputFile) || !File.Exists(inputFile))
 				{
-					Console.Error.WriteLine(Program.Help);
+					Console.Error.WriteLine(Program.Help, Assembly.GetExecutingAssembly().GetName().Version);
 					return;
 				}
 
@@ -44,7 +73,7 @@ namespace BuildTools.CssCompactor
 				using (output)
 				{
 					CssParser parser = new CssParser(inputFile);
-					parser.Write(output, CssOptions.None);
+					parser.Write(output, prettyPrint ? CssOptions.PrettyPrint : CssOptions.None);
 				}
 			}
 			catch (Exception ex)
