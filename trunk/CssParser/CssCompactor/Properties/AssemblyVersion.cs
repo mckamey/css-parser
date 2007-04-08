04fd2620469369d1c03090780d0a7967c98bd018
Stephen <Stephen@38bbe8e2-6fdc-11de-9e9f-05296fd93cd0>
2007-04-08 18:49:40 +0000

    - standardized AutoVersioning calls and AssemblyInfo

--- trunk/CssParser/CssCompactor/Properties/AssemblyVersion.cs
+++ trunk/CssParser/CssCompactor/Properties/AssemblyVersion.cs
@@ -1,7 +1,7 @@
-/*-------------------------------------------------------------*\
-    This is generated code.  All changes will be overwritten.
-\*-------------------------------------------------------------*/
+/*------------------------------------------------------------------*\
+	This is auto-generated code.  All changes will be overwritten.
+\*------------------------------------------------------------------*/
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.0703.2700")]
+[assembly: AssemblyVersion("1.0.0704.0811")]
